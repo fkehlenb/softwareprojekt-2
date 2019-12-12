4df9fcc3841add09f4f7212d4f4ddf740899c0cd
Leonard Haddad <s_xsipo6@uni-bremen.de>
2019-12-12 17:29:03 +0100

    added

---
+++ src/main/java/de/unibremen/sfb/control/OrderService.java
@@ -0,0 +1,43 @@
+package de.unibremen.sfb.control;
+
+import de.unibremen.sfb.entity.Order;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+
+
+@Singleton
+public class OrderService {
+
+	private List<Order> orders;
+
+	@PostConstruct
+	public void init() {
+		this.orders = createDefaultOrder();
+	}
+
+	@Schedule(hour = "*", minute = "*/10", persistent = false)
+	public void resetCustomers() {
+		this.orders = createDefaultOrder();
+	}
+
+	private List<Order> createDefaultOrder() {
+		List<Order> result = new ArrayList<>();
+		result.add(new Order("999-123456789", "DUMMY-DUMMY", "Laptop", 4, new Date(), false));
+		return result;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void addOrder(Order order) {
+		this.orders.add(order);
+	}
+
+}
