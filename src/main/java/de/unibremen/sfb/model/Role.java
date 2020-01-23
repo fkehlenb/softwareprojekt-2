a8ad701653e8352f9817613116448b1d73420e96
Liam Hurwitz <hurwitz@uni-bremen.de>
2020-01-23 20:33:22 +0100

    merg 2

--- src/main/java/de/unibremen/sfb/model/Role.java
+++ src/main/java/de/unibremen/sfb/model/Role.java
@@ -1,5 +1,7 @@
 package de.unibremen.sfb.model;
 
+import javax.persistence.ManyToMany;
+
 /** The roles a user can have*/
 public enum Role {
     TECHNOLOGE, PKADMIN, TRANSPORT, LOGISTIKER, ADMIN
--- src/main/java/de/unibremen/sfb/model/User.java
+++ src/main/java/de/unibremen/sfb/model/User.java
@@ -1,21 +1,17 @@
-package de.unibremen.sfb.Model;
+package de.unibremen.sfb.model;
 
-
+import lombok.Data;
 import lombok.NonNull;
 
-<<<<<<< HEAD:src/main/java/de/unibremen/sfb/model/User.java
 import javax.persistence.*;
-=======
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
->>>>>>> adminBean persistence workin:src/main/java/de/unibremen/sfb/Model/User.java
 import java.util.Date;
 import java.util.Set;
 
 /** This class is used to create user objects */
-<<<<<<< HEAD:src/main/java/de/unibremen/sfb/model/User.java
 @Data
 @Entity
 @NamedQueries({
@@ -34,48 +30,43 @@ public class User {
 public class User {
 
     /** User's id  */
-    //@NonNull
->>>>>>> adminBean persistence workin:src/main/java/de/unibremen/sfb/Model/User.java
     @Id
+    @NonNull
     public int id;
 
     /** The user's name */
-    //@NonNull
+    @NonNull
     public String vorname;
 
     /** The user's surname */
-    //@NonNull
+    @NonNull
     public String nachname;
 
     /** User's email address */
-    //@NonNull
+    @NonNull
     public String email;
 
     /** The user's phone number */
-    //@NonNull
+    @NonNull
     public String telefonnummer;
 
     /** User's username */
-    //@NonNull
+    @NonNull
     public String username;
 
     /** User's hashed password */
-<<<<<<< HEAD:src/main/java/de/unibremen/sfb/model/User.java
     @NonNull
-=======
->>>>>>> adminBean persistence workin:src/main/java/de/unibremen/sfb/Model/User.java
     public byte[] password;
 
     /** Is the user verified? */
-    //@NonNull
+    @NonNull
     public boolean wurdeVerifiziert;
 
     /** Creation date of the user object */
-    //@NonNull
+    @NonNull
     public Date erstellungsDatum;
 
     /** The role's of the user */
-<<<<<<< HEAD:src/main/java/de/unibremen/sfb/model/User.java
     @NonNull
     @OneToMany
     public Set<Role> rollen;
@@ -88,108 +79,13 @@ public class User {
     @NonNull
     @OneToMany
     public Set<Auftrag> auftraege;
-=======
-    //@NonNull
-    //public Set<Role> rollen;
-
-    /** The experimenting stations a user is assigned to */
-    //public Set<ExperimentierStation> stationen;
-
-    /** The jobs a user has */
-
-    //public Set<Auftrag> auftraege;
->>>>>>> adminBean persistence workin:src/main/java/de/unibremen/sfb/Model/User.java
 
     /** The User's language preference */
-    //@NonNull
+    @NonNull
     public String language;
 
-<<<<<<< HEAD:src/main/java/de/unibremen/sfb/model/User.java
     /** Empty Constructor */
     public User(){
         /* Required by jpa */
-=======
-    public User() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getVorname() {
-        return vorname;
-    }
-
-    public void setVorname(String vorname) {
-        this.vorname = vorname;
-    }
-
-    public String getNachname() {
-        return nachname;
-    }
-
-    public void setNachname(String nachname) {
-        this.nachname = nachname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefonnummer() {
-        return telefonnummer;
-    }
-
-    public void setTelefonnummer(String telefonnummer) {
-        this.telefonnummer = telefonnummer;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public byte[] getPassword() {
-        return password;
-    }
-
-    public void setPassword(byte[] password) {
-        this.password = password;
-    }
-
-    public boolean isWurdeVerifiziert() {
-        return wurdeVerifiziert;
-    }
-
-    public void setWurdeVerifiziert(boolean wurdeVerifiziert) {
-        this.wurdeVerifiziert = wurdeVerifiziert;
-    }
-
-    public Date getErstellungsDatum() {
-        return erstellungsDatum;
-    }
-
-    public void setErstellungsDatum(Date erstellungsDatum) {
-        this.erstellungsDatum = erstellungsDatum;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
->>>>>>> adminBean persistence workin:src/main/java/de/unibremen/sfb/Model/User.java
     }
 }
