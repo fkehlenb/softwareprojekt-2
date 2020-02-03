7230662b7bb5c6928584f7249d0e7f39a24d45fb
Leonard Haddad <s_xsipo6@uni-bremen.de>
2020-02-03 16:14:05 +0100

    .

--- src/main/java/de/unibremen/sfb/persistence/ExperimentierStationDAO.java
+++ src/main/java/de/unibremen/sfb/persistence/ExperimentierStationDAO.java
@@ -6,16 +6,21 @@ import de.unibremen.sfb.model.ExperimentierStation;
 
 import java.util.List;
 
-/** This class handles the experimenting station objects in the database */
+/**
+ * This class handles the experimenting station objects in the database
+ */
 public class ExperimentierStationDAO extends ObjectDAO<ExperimentierStation> {
 
-    /** Add an experimenting station object to the database
+    /**
+     * Add an experimenting station object to the database
+     *
      * @param es - the experimenting station to add to the database
-     * @throws DuplicateExperimentierStationException if the experimenting station already exists in the database */
-    public void persist(ExperimentierStation es) throws DuplicateExperimentierStationException{
-        if (es!=null){
-            synchronized (ExperimentierStation.class){
-                if (em.contains(es)){
+     * @throws DuplicateExperimentierStationException if the experimenting station already exists in the database
+     */
+    public void persist(ExperimentierStation es) throws DuplicateExperimentierStationException {
+        if (es != null) {
+            synchronized (ExperimentierStation.class) {
+                if (em.contains(es)) {
                     throw new DuplicateExperimentierStationException();
                 }
             }
@@ -23,64 +28,73 @@ public class ExperimentierStationDAO extends ObjectDAO<ExperimentierStation> {
         }
     }
 
-    /** Updates an existing experimenting station object in the database
+    /**
+     * Updates an existing experimenting station object in the database
+     *
      * @param es - the experimenting station to update in the database
-     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database */
-    public void update(ExperimentierStation es) throws ExperimentierStationNotFoundException{
-        if (es!=null){
-            if (!em.contains(es)){
+     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database
+     */
+    public void update(ExperimentierStation es) throws ExperimentierStationNotFoundException {
+        if (es != null) {
+            if (!em.contains(es)) {
                 throw new ExperimentierStationNotFoundException();
             }
             em.merge(es);
         }
     }
 
-    /** Removes an existing experimenting station object from the database
+    /**
+     * Removes an existing experimenting station object from the database
+     *
      * @param es - the experimenting station to remove from the database
-     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database */
-    public void remove(ExperimentierStation es) throws ExperimentierStationNotFoundException{
-        if (es!=null){
-            if (!em.contains(es)){
+     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database
+     */
+    public void remove(ExperimentierStation es) throws ExperimentierStationNotFoundException {
+        if (es != null) {
+            if (!em.contains(es)) {
                 throw new ExperimentierStationNotFoundException();
             }
             em.remove(es);
         }
     }
 
-    /** @return the class of experimenting stations */
-    public Class<ExperimentierStation> get(){
+    /**
+     * @return the class of experimenting stations
+     */
+    public Class<ExperimentierStation> get() {
         return ExperimentierStation.class;
     }
 
-    /** Get an experimenting station object from the database using its unique id
+    /**
+     * Get an experimenting station object from the database using its unique id
+     *
      * @param id - the unique id of the requested experimenting station
      * @return the experimenting station object which's id matches the entered one
-     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database */
-    public ExperimentierStation getObjById(int id) throws ExperimentierStationNotFoundException{
-        try{
-            ExperimentierStation es = em.find(get(),id);
-            if (es==null){
+     * @throws ExperimentierStationNotFoundException if the experimenting station couldn't be found in the database
+     */
+    public ExperimentierStation getObjById(int id) throws ExperimentierStationNotFoundException {
+        try {
+            ExperimentierStation es = em.find(get(), id);
+            if (es == null) {
                 throw new ExperimentierStationNotFoundException();
             }
             return es;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw new ExperimentierStationNotFoundException();
         }
     }
 
-    public List<ExperimentierStation> getAll(){
-        try {
-            List<ExperimentierStation> es = em.createQuery("SELECT ExperimentierStation FROM ExperimentierStation es",get()).getResultList();
-            if (es.isEmpty()){
-                throw new Exception();
-            }
-            return es;
-        }
-        catch (Exception e){
-            e.printStackTrace();
+    /**
+     * Get a list of all experimenting stations in the database
+     * @return a list of all experimenting stations
+     * @throws IllegalArgumentException if the list is empty
+     */
+    public List<ExperimentierStation> getAll() throws IllegalArgumentException {
+        List<ExperimentierStation> es = em.createNamedQuery("ExperimentierStation.getAll", get()).getResultList();
+        if (es.isEmpty()) {
             throw new IllegalArgumentException();
         }
+        return es;
     }
 }
