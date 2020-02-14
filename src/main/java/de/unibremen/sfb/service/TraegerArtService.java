3fc69cb173d3f1de13a19cd26caea53582ce676b
Leonard <s_xsipo6@uni-bremen.de>
2020-02-14 14:04:48 +0100

    .

---
+++ src/main/java/de/unibremen/sfb/service/TraegerArtService.java
@@ -0,0 +1,62 @@
+package de.unibremen.sfb.service;
+
+import de.unibremen.sfb.exception.DuplicateTraegerArtException;
+import de.unibremen.sfb.exception.TraegerArtNotFoundException;
+import de.unibremen.sfb.model.TraegerArt;
+import de.unibremen.sfb.persistence.TraegerArtDAO;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.List;
+
+/** This class manages the business logic for container types */
+public class TraegerArtService {
+
+    /** TraegerArt DAO */
+    @Inject
+    private TraegerArtDAO traegerArtDAO;
+
+    /** TraegerArt */
+    private List<TraegerArt> traegerArt;
+
+    /** Get all container types from the database
+     * @return a list of all container types in the database */
+    public List<TraegerArt> getAll(){
+        return traegerArtDAO.getAll();
+    }
+
+    /** Add a new container type to the database
+     * @param ta - the container type to add
+     * @throws DuplicateTraegerArtException on failure */
+    public void addTraegerArt(TraegerArt ta) throws DuplicateTraegerArtException {
+        traegerArtDAO.persist(ta);
+    }
+
+    /** Update a container type in the database
+     * @param ta - the container type to update
+     * @throws TraegerArtNotFoundException on failure */
+    public void updateTragerArt(TraegerArt ta) throws TraegerArtNotFoundException {
+        traegerArtDAO.update(ta);
+    }
+
+    /** Remove a container type from the database
+     * @param ta - the container type to remove
+     * @throws TraegerArtNotFoundException on failure */
+    public void removeTraegerArt(TraegerArt ta) throws TraegerArtNotFoundException{
+        traegerArtDAO.remove(ta);
+    }
+
+    /** Get a container type by name
+     * @param taName - the container type name
+     * @throws TraegerArtNotFoundException on failure */
+    public TraegerArt getByName(String taName) throws TraegerArtNotFoundException{
+        return traegerArtDAO.getByName(taName);
+    }
+
+    /** Get a container type by id
+     * @param id - the container type id
+     * @throws TraegerArtNotFoundException on failure */
+    public TraegerArt getById(int id) throws TraegerArtNotFoundException{
+        return traegerArtDAO.getById(id);
+    }
+}
