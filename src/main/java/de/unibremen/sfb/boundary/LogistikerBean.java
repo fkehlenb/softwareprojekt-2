0e5dd279854327f1928a640076d56169c0f26bc8
Leonard <s_xsipo6@uni-bremen.de>
2020-02-25 17:13:25 +0100

    traeger service

--- src/main/java/de/unibremen/sfb/boundary/LogistikerBean.java
+++ src/main/java/de/unibremen/sfb/boundary/LogistikerBean.java
@@ -4,6 +4,7 @@ import de.unibremen.sfb.model.*;
 import de.unibremen.sfb.persistence.ProbeDAO;
 import de.unibremen.sfb.service.AuftragService;
 import de.unibremen.sfb.service.ProzessKettenVorlageService;
+import de.unibremen.sfb.service.TraegerService;
 import de.unibremen.sfb.service.ZustandsService;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,28 +33,36 @@ import static de.unibremen.sfb.model.ProzessKettenZustandsAutomat.GESTARTET;
 @Setter
 @Slf4j
 public class LogistikerBean implements Serializable {
-private List<Probe> proben;
-private List<Auftrag> auftrage;
+    private List<Probe> proben;
+    private List<Auftrag> auftrage;
 
-@Inject
-ProbeDAO probeDAO;
+    @Inject
+    private ProbeDAO probeDAO;
 
-@Inject
-AuftragService auftragService;
+    @Inject
+    private AuftragService auftragService;
 
-@Inject
-ProzessKettenVorlageService prozessKettenVorlageService;
+    @Inject
+    private ProzessKettenVorlageService prozessKettenVorlageService;
 
-@Inject
-ZustandsService zustandsService;
+    @Inject
+    private ZustandsService zustandsService;
 
-@Inject
-AuftragView auftragView;
+    @Inject
+    private AuftragView auftragView;
 
-@PostConstruct
+    /** Traeger service */
+    @Inject
+    private TraegerService traegerService;
+
+    /** All containers */
+    private List<Traeger> traegers;
+
+    @PostConstruct
     void init() {
-    auftrage = auftragService.getAuftrage();
-    proben = getProben();
+        auftrage = auftragService.getAuftrage();
+        proben = getProben();
+        traegers = getTraegerList();
     }
 
     /**
@@ -63,89 +72,114 @@ AuftragView auftragView;
 
     /**
      * returns all carriers currently existing
-     * @return a set containing all carriers
+     *
+     * @return a set containing all containers
      */
-    public Set<Traeger> getTraegerList() { return null; }
+    public List<Traeger> getTraegerList() {
+        return traegerService.getAll();
+    }
 
     /**
      * creates a new carrier
-     * @return the new carrier
      */
-    public Traeger createTraeger() { return null; }
+    public void createTraeger() {
+
+    }
+
+    /** Update a container
+     * @param id - the id of the container to update */
+    public void updateTraeger(int id){
+
+    }
 
     /**
      * deletes a carrier
-     * @param t the carrier to be deleted
+     * @param id - the id of the container to remove
      */
-    public void deleteTraeger(Traeger t) {}
+    public void deleteTraeger(int id) {
+    }
 
     /**
      * returns all jobs currently awaiting processing by the logistic
+     *
      * @return a set containing all jobs currently awaiting processing
      */
-    public Set<Auftrag> getAuftrag() { return null; }
+    public Set<Auftrag> getAuftrag() {
+        return null;
+    }
 
     /**
      * assigns a sample to a job
+     *
      * @param a the job
      * @param p the sample
      */
-    public void zuorndnenProbe(Auftrag a, Probe p) {}
+    public void zuorndnenProbe(Auftrag a, Probe p) {
+    }
 
     /**
      * assigns a carrier to a job
+     *
      * @param a the job
      * @param t the carrier
      */
-    public void zuordnenTraeger(Auftrag a, Traeger t) {}
+    public void zuordnenTraeger(Auftrag a, Traeger t) {
+    }
 
     /**
      * returns all samples currently existing
+     *
      * @return a set containing all samples
      */
     public List<Probe> getProben() {
-        return probeDAO.getProben(1,100);
+        return probeDAO.getProben(1, 100);
     }
 
     /**
      * returns all samples currently existing by whether or not they are archived
+     *
      * @param archiviert True: display archived samples. False: display samples which are not archived
      * @return a set containing all samples which fullfill the condition
      */
-    public Set<Probe> getProben(boolean archiviert) { return null; }
+    public Set<Probe> getProben(boolean archiviert) {
+        return null;
+    }
 
     /**
      * returns the current location of a sample
+     *
      * @param p the sample
      * @return the location
      */
-    public Standort getProbenStandort(Probe p) { return null; }
+    public Standort getProbenStandort(Probe p) {
+        return null;
+    }
 
     /**
      * starts a job
+     *
      * @param auftrag the job to be started
      */
     public void startAuftrag(int auftrag) {
         try {
             Auftrag a = auftragService.getAuftrag(auftrag);
-            auftragService.zustandswechsel(a, GESTARTET );
+            auftragService.zustandswechsel(a, GESTARTET);
             log.info("Auftrag wurde gestartet! ID: " + auftrag);
             facesNotification("Auftrag wurde gestartet! ID: " + auftrag);
             //Aktualisiert Auftragsliste
             auftragView.updateAuftragTabelle();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             log.error("Failed to change auftrag state! ID: " + auftrag);
             facesError("Failed to change auftrag state! ID: " + auftrag);
         }
 
 
-
     }
 
     /**
      * refuses a job (signals to the process chain administrator that this job cannot be started in the current form)
+     *
      * @param auftrag the job
      * @param message the message to the process chain administrator
      */
@@ -163,41 +197,57 @@ AuftragView auftragView;
             facesError("Failed to change auftrag state! ID: " + auftrag);
         }
     }
+
     /**
      * returns an error message
+     *
      * @return the error message
      */
-    public String errorMessage() { return null; }
+    public String errorMessage() {
+        return null;
+    }
 
     /**
      * assigns himself to a job so that no other logistic expert starts working on it
+     *
      * @param a the job
      */
-    public void zuordnen(Auftrag a) {}
+    public void zuordnen(Auftrag a) {
+    }
 
     /**
      * get's specific samples and their amount
+     *
      * @param a the job
      * @return the Pair of samples with the amount.
      */
-    public Set<Pair<QualitativeEigenschaft,Integer>> getAngeforderteProben(Auftrag a){return null;}
+    public Set<Pair<QualitativeEigenschaft, Integer>> getAngeforderteProben(Auftrag a) {
+        return null;
+    }
 
     /**
      * the empty constructor
      */
-    public LogistikerBean() {}
+    public LogistikerBean() {
+    }
 
     /**
      * returns the logistic expert managed by this bean
+     *
      * @return the user
      */
-    public User getLogistiker() { return logistiker; }
+    public User getLogistiker() {
+        return logistiker;
+    }
 
     /**
      * sets the logistic expert managed by this bean
+     *
      * @param logistiker the user
      */
-    public void setLogistiker(User logistiker) { this.logistiker = logistiker; }
+    public void setLogistiker(User logistiker) {
+        this.logistiker = logistiker;
+    }
 
     /**
      * Adds a new SEVERITY_ERROR FacesMessage for the ui
