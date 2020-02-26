3553f644b5b634f394c1d77d69a4b124903520e7
Liam <liam@contra-bit.com>
2020-02-26 14:02:28 +0100

    merge Translation

--- src/main/java/de/unibremen/sfb/model/ProzessSchrittVorlage.java
+++ src/main/java/de/unibremen/sfb/model/ProzessSchrittVorlage.java
@@ -29,11 +29,11 @@ public class ProzessSchrittVorlage {
     private String dauer;
 
     /** Accepted container input types */
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<TraegerArt> eingabeTraeger;
 
     /** Accepted container output types */
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<TraegerArt> ausgabeTraeger;
 
     /** The process step type */
