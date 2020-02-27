5d354728a38b337d49e63084f5ca01ce7163b3a0
Liam <liam@contra-bit.com>
2020-02-27 17:34:33 +0100

    remove unsed dependency

--- src/test/java/de/unibremen/sfb/Selenium/QuantitativEingeschaftSelenium.java
+++ src/test/java/de/unibremen/sfb/Selenium/QuantitativEingeschaftSelenium.java
@@ -3,7 +3,6 @@ package de.unibremen.sfb.Selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class QuantitativEingeschaftSelenium {
