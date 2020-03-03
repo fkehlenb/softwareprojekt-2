2f91b738b313a0b6a558b3ab6e2fe05131c3713c
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-03-03 16:47:25 +0100

    newSetup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "You have choosen following Folder to replace the StandAlone.xml Datai" "$1"
+echo "You have choosen following Folder to replace the Standalone.xml File" "$1"
 
 cp standalone.xml $1
