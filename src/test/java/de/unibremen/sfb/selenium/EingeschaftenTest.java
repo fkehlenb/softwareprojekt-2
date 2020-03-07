2cd14da18c15c424e4e8557db68f76083b17d28b
Liam <liam@contra-bit.com>
2020-03-07 19:38:08 +0100

    pom und test

--- src/test/java/de/unibremen/sfb/selenium/EingeschaftenTest.java
+++ src/test/java/de/unibremen/sfb/selenium/EingeschaftenTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Web_driver;
-import org.openqa.selenium.phantomjs.PhantomJS_driver;
-import org.openqa.selenium.phantomjs.PhantomJS_driverService;
+import org.openqa.selenium.Dimension;;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
+
 
 /**
  * <p>EingeschaftenTest class.</p>
@@ -22,50 +23,52 @@ import java.util.concurrent.TimeUnit;
  * @since 1.0
  */
 public class EingeschaftenTest {
-    private Web_driver _driver;
-    private String spoofUserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36";
+private WebDriver driver;
+	private String spoofUserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36";
+    /**
+     * This Class Shows, that even when correctly configured. The command  new PhantomJSDriver(); is buggy
+     * Stack Overflow recommends updating the PhantomJS and Selenium to current Version
+     * @return the Driver
+     */
+	public WebDriver getDriver() {
+		if (this.driver == null) {
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			caps.setCapability(
+					PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX
+							+ "userAgent", spoofUserAgent);
 
-    protected Web_driver get_driver() {
-        if (this.__driver == null) {
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setJavascriptEnabled(true);
-            caps.setCapability(
-                    PhantomJS_driverService.PHANTOMJS_PAGE_SETTINGS_PREFIX
-                            + "userAgent", spoofUserAgent);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,
+					new String[] { "--web-security=false",
+							"--ssl-protocol=any", "--ignore-ssl-errors=true",
+							"--webdriver-loglevel=INFO" });
 
-            caps.setCapability(PhantomJS_driverService.PHANTOMJS_CLI_ARGS,
-                    new String[]{"--web-security=false",
-                            "--ssl-protocol=any", "--ignore-ssl-errors=true",
-                            "--web_driver-loglevel=INFO"});
-
-            PhantomJS_driverService service = new PhantomJS_driverService.Builder()
-                    .usingPort(8080)
-                    .usingPhantomJSExecutable(new File("../phantomjs-2.1.1-linux-x86_64"))
-                    .build();
-            this.__driver = new PhantomJS_driver(service, caps);
-        }
-
-        return this.__driver;
-    }
+			PhantomJSDriverService service = new PhantomJSDriverService.Builder()
+			        .usingPort(8081)
+			        .usingPhantomJSExecutable(new File("/usr/bin/phantomjs"))
+			        .build();
+			this.driver = new PhantomJSDriver(service, caps);
+		}
+		return this.driver;
+	}
 
     /**
      * <p>setUp.</p>
      */
     @BeforeEach
     public void setUp() {
+        driver = getDriver();
         // Download link is http://phantomjs.org/download.html
         // Set implicit wait
-        _driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        // Open "Google.com and search SoftwareTestingMaterial.com"
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    /**
-     * <p>tearDown.</p>
-     */
     @AfterEach
     public void tearDown() {
-        _driver.quit();
+        driver.close();
     }
+
+
 
     /**
      * <p>eingeschaftenTest.</p>
@@ -75,39 +78,39 @@ public class EingeschaftenTest {
     @Test
     public void eingeschaftenTest() throws InterruptedException {
         //Anmeldung
-        _driver.get("http://localhost:8080/");
-        _driver.manage().window().setSize(new Dimension(1440, 900));
-        _driver.findElement(By.xpath("//p/a")).click();
-        _driver.findElement(By.id("username")).click();
+        driver.get("http://localhost:8080/");
+        driver.manage().window().setSize(new Dimension(1440, 900));
+        driver.findElement(By.xpath("//p/a")).click();
+        driver.findElement(By.id("username")).click();
         Thread.sleep(700);
-        _driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("username")).sendKeys("admin");
         Thread.sleep(700);
-        _driver.findElement(By.id("password")).sendKeys("12345678");
+        driver.findElement(By.id("password")).sendKeys("12345678");
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//button/span")).click();
+        driver.findElement(By.xpath("//button/span")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//div[2]/div/h3")).click();
+        driver.findElement(By.xpath("//div[2]/div/h3")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//div[2]/div/div/ul/li[2]/a/span[2]")).click();
+        driver.findElement(By.xpath("//div[2]/div/div/ul/li[2]/a/span[2]")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//li[7]/a/span")).click();
+        driver.findElement(By.xpath("//li[7]/a/span")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//td[2]/input")).click();
+        driver.findElement(By.xpath("//td[2]/input")).click();
         Thread.sleep(700);
         //Formular
-        _driver.findElement(By.id("j_idt63:nameQl")).sendKeys("pruebaTest");
+        driver.findElement(By.id("j_idt63:nameQl")).sendKeys("pruebaTest");
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//td[4]/button/span")).click();
+        driver.findElement(By.xpath("//td[4]/button/span")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//td[3]/div/a/span")).click();
+        driver.findElement(By.xpath("//td[3]/div/a/span")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//div[2]/input")).click();
+        driver.findElement(By.xpath("//div[2]/input")).click();
         Thread.sleep(700);
-        _driver.findElement(By.id("form2:qEin:0:nameQl2")).sendKeys("other");
+        driver.findElement(By.id("form2:qEin:0:nameQl2")).sendKeys("other");
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//td[3]/div/a[2]/span")).click();
+        driver.findElement(By.xpath("//td[3]/div/a[2]/span")).click();
         Thread.sleep(700);
-        _driver.findElement(By.xpath("//button/span[2]")).click();
+        driver.findElement(By.xpath("//button/span[2]")).click();
         Thread.sleep(700);
     }
 }
